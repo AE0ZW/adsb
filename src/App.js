7b31d24ebc7e3796c9daf620d723dc81531efe22
Dennis Dunn <ansofive@gmail.com>
2020-02-03 13:15:01 -0700

    Added runtime configuration of the dump1090 host:port.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './app.css';
 import { FlightMap, FlightTable, TimeBoard } from './components';
-import { getAirports, poll,getCenter } from './services';
+import { getAirports, poll, getCenter } from './services';
+import { Overlay } from './components';
+import { ConfigForm } from './components';
 
 const defaultPosition = [39.833333, -98.583333]; // geographic center of the US
 
@@ -11,10 +13,15 @@ function App() {
     const [center, setCenter] = useState(defaultPosition);
     const [airports, setAirports] = useState([]);
     const [timezones, setTimezones] = useState(['UTC']);
+    const [overlay, setOverlay] = useState(true);
+    const [hostInfo, setHostInfo] = useState({ host: '192.168.0.0', port: 8080 });
+    const [isPolling, setIsPolling] = useState(false);
 
     useEffect(() => {
-        return poll(process.env.REACT_APP_DATA_URL, process.env.REACT_APP_DATA_PERIOD, setFlights);
-    }, []);
+        if (isPolling) {
+            return poll(`http://${hostInfo.host}:${hostInfo.port}/data.json`, 1000, setFlights);
+        }
+    }, [isPolling, hostInfo.host, hostInfo.port]);
 
     useEffect(() => {
         if (flights.length > 0 && airports.length === 0) {
@@ -34,6 +41,9 @@ function App() {
                 <TimeBoard timezones={timezones} />
                 <FlightTable headings={['Flight', 'Alt', 'Hdg', 'Spd']} flights={flights} />
             </div>
+            <Overlay visible={overlay}>
+                <ConfigForm defaultHost={hostInfo.host} defaultPort={hostInfo.port} onSubmit={formData => {console.log(formData); setHostInfo(formData); setIsPolling(true);setOverlay(false); }}></ConfigForm>
+            </Overlay>
         </div >
     );
 }
