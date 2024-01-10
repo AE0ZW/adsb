2910ada3f0eb747dee81cf9929c3f837178d50e3
Dennis Dunn <ansofive@gmail.com>
2024-01-09 20:26:17 -0500

    Changed location accuracy, set the station to John Glen Airport.

--- build/dump1090/entrypoint.sh
+++ build/dump1090/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo "LAT $STA_LAT"
-echo "LON $STA_LON"
+echo "Lat $STA_LAT"
+echo "Lon $STA_LON"
+echo "Location accuracy $lOC_ACCURACY"
 
-/usr/bin/dump1090 --net --quiet --write-json /data --lat $STA_LAT --lon $STA_LON
+/usr/bin/dump1090 --net --quiet --write-json /data --lat $STA_LAT --lon $STA_LON --json-location-accuracy $LOC_ACCURACY
 
