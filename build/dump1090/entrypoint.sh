df87f52baf7cd6833a84d3212cba8ad27345e6d5
Dennis Dunn <ansofive@gmail.com>
2024-01-19 21:10:21 -0500

    Moved the data directory for the json files.

--- build/dump1090/entrypoint.sh
+++ build/dump1090/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-echo "Lat $STA_LAT"
-echo "Lon $STA_LON"
-echo "Location accuracy $lOC_ACCURACY"
-
-/usr/bin/dump1090 --net --quiet --write-json /data --lat $STA_LAT --lon $STA_LON --json-location-accuracy $LOC_ACCURACY
+/usr/bin/dump1090 --net --quiet --write-json /run/dump1090 --lat $STA_LAT --lon $STA_LON --json-location-accuracy $LOC_ACCURACY
 
