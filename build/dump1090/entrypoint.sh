50f5a6e25b9a9859df3b5ea3605fb40613e8d59d
Dennis Dunn <ansofive@gmail.com>
2024-01-21 23:05:41 -0500

    Added the device index to the dump1090 call.

--- build/dump1090/entrypoint.sh
+++ build/dump1090/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/dump1090 --net --quiet --write-json /run/dump1090 --lat $STA_LAT --lon $STA_LON --json-location-accuracy $LOC_ACCURACY
+/usr/bin/dump1090 --net --quiet --device ${DEVICE} --write-json /run/dump1090 --lat $STA_LAT --lon $STA_LON --json-location-accuracy $LOC_ACCURACY
 
